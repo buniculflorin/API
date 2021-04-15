0b2795e64ff56cca4d992ff66797455174b7cb84
buniculflorin <buniculflorin@gmail.com>
2021-04-16 00:47:41 +0100

    Authentication token created

--- src/Controller/AuthToken.php
+++ src/Controller/AuthToken.php
@@ -3,8 +3,23 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
+/**
+ * Class AuthToken
+ *
+ * Endpoint for token provider
+ */
 class AuthToken
 {
+    public const TOKEN = "MpWUMGLUeg6FQQr6CHi7S8n9tfapY2bc";
 
+    /**
+     * @Route("/auth/token")
+     */
+    public function execute()
+    {
+        return new JsonResponse(['token' => self::TOKEN]);
+    }
 }
